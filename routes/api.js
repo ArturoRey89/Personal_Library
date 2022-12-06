859fe9e2fb134b104aeb773774f0b9a78ea1725b
ArturoRey89 <arturoreyes89@gmail.com>
2022-12-06 14:53:29 +0100

    feat: GET api/books/:id

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ const bookSchema = new mongoose.Schema({
   commentcount: { type: Number, default: 0 },
   comments: [String]
 });
-const Book = mongoose.model("Book", bookSchema);
+const Books = mongoose.model("Book", bookSchema);
 const ObjectId = mongoose.Types.ObjectId;
 
 module.exports = function (app) {
@@ -30,7 +30,7 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-      Book.find({}, (err, books) => {
+      Books.find({}, (err, books) => {
         if(err){
           res.status(500)
           console.log("error on find: ", err)
@@ -42,46 +42,69 @@ module.exports = function (app) {
     })
     
     .post(function (req, res){
-      if(req.body.title) {
-        let newBook = new Book({ title: req.body.title });
+      //response will contain new book object including atleast _id and title
+      let newTitle = req.body.title;
+      if (newTitle) {
+        let newBook = new Books({ title: newTitle });
         newBook.save((err) => {
-          if(err) console.log("Save error: ", err);
+          if (err){ 
+            res.status(500)
+            return console.log("Save error: ", err)
+          };
           res.json({ _id: newBook._id, title: newBook.title });
         });
-      }
-      else {
+      } else {
         res.send("missing required field title");
       }
-      //response will contain new book object including atleast _id and title
     })
     
     .delete(function(req, res){
-      Book.deleteMany({}, (err,res) => {
-        if(err){ 
-          return console.log("somthing went wrong when deleting")
+      //if successful response will be 'complete delete successful'
+      Book.deleteMany({}, (err, res) => {
+        if (err) {
+          res.status(500)
+          return console.log("somthing went wrong when deleting");
         }
         res.send("complete delete successful");
-      })
-      //if successful response will be 'complete delete successful'
+      });
     });
 
 
 
   app.route('/api/books/:id')
     .get(function (req, res){
-      let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      let bookId = req.params.id;
+      if (bookId) {
+        Books.findOne(
+        { _id: ObjectId (bookId)}, 
+        {__v:0, commentcount: 0},
+        (err, book) => {
+          if (err) {
+            res.status(500)
+            return console.log("findOne bookId error: ", err)
+          }
+          if(book) {
+            res.send(book)
+          } else {
+            res.send("no book exists");
+          }
+
+        });
+      } else {
+        res.send("missing required field comment");
+      }
     })
     
     .post(function(req, res){
+      //json res format same as .get
       let bookid = req.params.id;
       let comment = req.body.comment;
-      //json res format same as .get
     })
     
     .delete(function(req, res){
-      let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      let bookid = req.params.id;
     });
   
 };
